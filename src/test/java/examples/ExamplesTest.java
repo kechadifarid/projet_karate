17135da36307bb17e6bde301f8b8c404fceeb6fe
kechadifarid <kechadifarid10@gmail.com>
2026-08-31 16:16:27 +0200

    jenkins

--- src/test/java/examples/ExamplesTest.java
+++ src/test/java/examples/ExamplesTest.java
@@ -2,6 +2,7 @@ package examples;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import io.qameta.allure.karate.AllureKarate;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,7 @@ class ExamplesTest {
     @Test
     void testParallel() {
         Results results = Runner.path("classpath:examples")
+                .hook(new AllureKarate())
                 //.outputCucumberJson(true)
                 .parallel(5);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
